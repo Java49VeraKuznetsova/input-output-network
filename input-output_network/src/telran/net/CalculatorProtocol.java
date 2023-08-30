af25841a39a67549c66a9e41b744b920a6cee86d
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2023-08-30 17:33:25 +0300

    HW 92(41) Yuri

--- input-output_network/src/telran/net/CalculatorProtocol.java
+++ input-output_network/src/telran/net/CalculatorProtocol.java
@@ -1,11 +1,30 @@
 package telran.net;
 
+
 public class CalculatorProtocol implements ApplProtocol {
 
 	@Override
 	public Response getResponse(Request request) {
-		// TODO Auto-generated method stub
-		return null;
+		Response response = null;
+		
+		try {
+			double[] operands = (double[])request.requestData();
+			String requestType = request.requestType();
+			double res = switch(requestType) {
+			case "add" -> operands[0] + operands[1];
+			case "multiply" -> operands[0] * operands[1];
+			case "divide" -> operands[0] / operands[1];
+			case "minus" -> operands[0] - operands[1];
+			default -> Double.NaN;
+			};
+			response = Double.isNaN(res) ?
+					new Response(ResponseCode.WRONG_TYPE, requestType + " is wrong request type"):
+						new Response(ResponseCode.OK, res);
+			return response;
+		} catch (Exception e) {
+			return new Response(ResponseCode.WRONG_DATA, e.getMessage());
+		}
 	}
 
+	
 }
--- input-output_network/src/telran/net/CalculatorTcpClientAppl.java
+++ input-output_network/src/telran/net/CalculatorTcpClientAppl.java
@@ -1,10 +1,6 @@
 package telran.net;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.net.Socket;
+
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -17,24 +13,19 @@ public class CalculatorTcpClientAppl {
 	static final String HOST="localhost";
 	static final int PORT=5000;
 		public static void main(String[] args) throws Exception{
-			try(Socket socket = new Socket(HOST, PORT);
-					PrintStream writer = new PrintStream(socket.getOutputStream());
-					BufferedReader reader =
-							new BufferedReader(new InputStreamReader(socket.getInputStream()))){
+			try(TcpHandler tcpHandler = new TcpHandler(HOST, PORT);){
 				 InputOutput io = new ConsoleInputOutput();
 				 Menu menu = new Menu("Calculator Application", Item.of("send request", io1 -> {
 					 HashSet<String> requests = new HashSet<>(Arrays.asList("add", "minus",
 							 "multiply", "divide"));
-					 String requestType = io1.readString("Enter operation type " + requests, "Wrong operation", requests);
+					 String requestType = io1.readString("Enter operation type " + requests,
+							 "Wrong operation", requests);
 					 double op1 = io1.readDouble("Enter first number", "Wrong number");
 					 double op2 = io1.readDouble("Enter second number", "Wrong number");
-					 writer.println(String.format("%s#%f#%f", requestType, op1,op2));
-					 try {
-						String response = reader.readLine();
-						io1.writeLine(response);
-					} catch (IOException e) {
-						throw new RuntimeException(e.toString());
-					}
+					 double res = tcpHandler.send(requestType, new double[] {op1, op2});
+					 io1.writeLine(res);
+					 
+					 
 				 }), Item.ofExit());
 				 menu.perform(io);
 			}
