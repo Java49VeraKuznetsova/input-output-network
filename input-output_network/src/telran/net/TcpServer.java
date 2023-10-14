f9180f90660f5619b6d9bec1acf2201caf87912c
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2023-10-14 21:02:34 +0300

    HW 101(50) done vers2

--- input-output_network/src/telran/net/TcpServer.java
+++ input-output_network/src/telran/net/TcpServer.java
@@ -9,55 +9,36 @@ public class TcpServer implements Runnable {
 	private int port;
 	private ApplProtocol protocol;
 	private ServerSocket serverSocket;
-	/*
-	private int cores;
-	private ExecutorService threadPool;
-	private BlockingQueue<TcpClientServer> queue;
-	*/
+	
 	private int cores = Runtime.getRuntime().availableProcessors();
 	private ExecutorService threadPool = Executors.newFixedThreadPool(cores);
-	private BlockingQueue<TcpClientServer> queue = new LinkedBlockingQueue<>();
+	
 	
 	public TcpServer(int port, ApplProtocol protocol) throws IOException {
 		this.port = port;
 		this.protocol = protocol;
 		serverSocket = new ServerSocket(port);
-		/*
-		cores = Runtime.getRuntime().availableProcessors();
-		ExecutorService threadPool = Executors.newFixedThreadPool(cores);
-		BlockingQueue<TcpClientServer> queue = new LinkedBlockingQueue<>();
-		*/
+	
 	}
 
 	@Override
 	public void run() {
 		System.out.println("Server is listening on port " + port);
+		System.out.println("cores " + cores);
+		while(true) {
 		try {
-			while(true) {
+			
 				Socket socket = serverSocket.accept();
 				TcpClientServer clientServer = new TcpClientServer(socket, protocol);
-			    queue.put(clientServer);
-				try {
-					TcpClientServer clientServerCurrent = queue.take();
-					threadPool.execute(clientServerCurrent);
+			 			threadPool.execute(clientServer);
 					
-				} catch (InterruptedException e){
-					TcpClientServer clientServerReminder = null;
-					while ((clientServerReminder = queue.poll()) != null) {
-						threadPool.execute(clientServerReminder);
-					}
-					break;
-				}
-				
 			
-				//Thread thread = new Thread (clientServer);
-				//thread.start();
-				//clientServer.run();
-			}
 			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-	}
-
+	
+		}
 }
+	}
+		
